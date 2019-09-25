14049c4b049a83a0e3c732216086a4568bdaa8be
Vigneshwar Ravichandran <vigneshwarravichandran@gmail.com>
2019-09-26 00:54:09 +0530

    Completed Login Process
    Signed-off-by: Vigneshwar Ravichandran <vigneshwarravichandran@gmail.com>

---
+++ website/login_system/helper.py
@@ -0,0 +1,13 @@
+from .models import *
+
+def user_exists(username):
+	user = Users.objects.filter(username=username)
+	if user:
+		return True
+	return False
+
+def valid_credentials(username, password):
+	user = Users.objects.get(username=username)
+	if user.password == password:
+		return True
+	return False
--- website/login_system/views.py
+++ website/login_system/views.py
@@ -1,11 +1,34 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .helper import *
 
 def login(request):
-	return render(request, 'login.html')
+	context = {
+		'error' : None,
+		'username' : None,
+		'user_id' : None
+	}
+	if request.method == 'POST':
+		username = request.POST.get('username')
+		password = request.POST.get('password')
+		error = None
+		if user_exists(username):
+			if valid_credentials(username, password):
+				error = 'Success'
+			else:
+				error = 'Invalid password'
+		else:
+			error = 'Invalid username'
+		context['error'] = error
+		return render(request, 'user.html', context)
+	return render(request, 'login.html', context)
 
 def register(request):
-	return HttpResponse('Register')
+	if request.method == 'POST':
+		username = request.POST.get('username')
+		password = request.POST.get('password')
+		return HttpResponse('username {0} password {1}'.format(username, password))
+	return render(request, 'register.html')
 
 def user(request):
 	return HttpResponse('User')
